5502961c952f00d56bfeb8a7a3b4d7db46292526
RazzkaRusmantoro <razzkarusmantoro123@gmail.com>
2025-01-18 23:28:29 +0400

    Fixes

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are served with relative paths
+  base: './',
+  build: {
+    outDir: 'dist',  // Specify the output directory if not already set
+    rollupOptions: {
+      input: '/src/main.jsx',  // Point to the main entry file
+    },
+  },
 });
