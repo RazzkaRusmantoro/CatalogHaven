af87ba10b632944a9228908947c86f1d77d0d277
RazzkaRusmantoro <razzkarusmantoro123@gmail.com>
2025-01-19 00:34:05 +0400

    Deployment tests

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './', // Adjust this path if your index.html is in a different location
+  base: '/cataloghaven/',
   plugins: [react()],
 });
